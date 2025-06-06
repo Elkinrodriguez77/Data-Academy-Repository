4d7a954bd5666a9f615d00760f2eb0f297785b84
Elkin Rodriguez <elkin.rodrigueze@outlook.com>
2025-06-06 11:42:46 -0500

    actualizacion rep

---
+++ SQL/Ejercicios de Consultas/Consultas taller Netflix.sql
@@ -0,0 +1,63 @@
+-- Consultas Básicas
+--Seleccionar todos los datos:
+
+-- 1. Muestra todas las filas de la tabla netflix_data.
+
+
+
+-- 2. ¿Cuántos registros corresponden a películas (type = 'Movie')?
+
+
+
+-- 3. Lista los títulos de contenido producido en India.
+
+
+
+-- 4. Encuentra todos los títulos que contienen la palabra "Love" en su nombre.
+
+
+
+-- 5. Obtén los títulos con clasificación TV-MA.
+
+
+
+-- Consultas nivel medio
+
+-- 6. Muestra los títulos y la fecha en que fueron añadidos, ordenados por fecha de manera descendente.
+
+
+
+-- 7. ¿Cuántos títulos hay por tipo de contenido (media_type)?
+
+
+
+-- 8. Encuentra los países con la mayor cantidad de títulos, ordenados de mayor a menor.
+
+
+
+-- 9. Muestra las películas cuya duración es mayor a 100 minutos (asegurándote de filtrar solo media_type = 'Movie').
+
+
+
+-- 10. Lista los títulos que se produjeron en United States e India.
+
+
+
+-- 11, ¿Cuáles son los géneros más frecuentes en la columna listed_in?
+
+
+
+-- 12. Encuentra los 10 títulos añadidos más recientemente.
+
+
+
+-- 13. ¿Cuántos títulos fueron lanzados en el año 2020?
+
+
+
+-- 14. Calcula la duración promedio (en minutos) de las películas.
+
+
+
+-- 15. Muestra el total de títulos por cada director.
+
---
+++ SQL/Ejercicios de Consultas/Crear Tabla Netflix.sql
@@ -0,0 +1,14 @@
+CREATE TABLE netflix_data (
+    show_id TEXT PRIMARY KEY,
+    media_type TEXT,
+    title TEXT,
+    director TEXT,
+    cast_members TEXT,
+    country TEXT,
+    date_added TEXT,
+    release_year INT,
+    rating TEXT,
+    duration TEXT,
+    listed_in TEXT,
+    description TEXT
+);
